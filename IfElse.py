a5acbb4ecb2805c82cba857d7fe8e147c7630ffc
Franklin Guevara <tecguevara@gmail.com>
2020-07-25 00:32:11 -0600

    first commit(commit to tutofiles)\

---
+++ IfElse.py
@@ -0,0 +1,9 @@
+x = "Spathiphyllum"
+y = input("Enter the name of your plan: ")
+if x == y:
+    print("Yes - Spathiphyllum is the best plan ever!")
+elif x == "spathiphyllum":
+    print("No, I want a big Spathiphyllum!")
+else:
+    print("Spathiphyllum! Not " + y + "!")
+    
