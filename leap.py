5274112acc04d97e9e8883150b9ba527634b5ee1
Franklin Guevara <tecguevara@gmail.com>
2020-07-25 01:08:00 -0600

    finished leap (commit to tutofiles)

--- leap.py
+++ leap.py
@@ -1,1 +1,15 @@
-print("Testing commits")
+year = int(input("Enter a year: "))
+if year >= 1582:
+    if (year % 4) != 0:
+        if (year % 400) != 0:
+            typeyear = "common year"
+        elif (year % 100) !=0:
+            typeyear = "Leap year"
+        else:
+            pass
+    else:
+        typeyear = "Leap year"
+    print("The year you have input is: " + typeyear)
+else:
+    print("Not within the Gregorian Calendar period")
+
