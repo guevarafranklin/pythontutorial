b50bbcc55a4b6ae13d7669938ca57f3412102def
tecguevara <tecguevara@gmail.com>
2020-09-16 10:00:52 -0600

    adding test2.py  to the repo

---
+++ test2.py
@@ -0,0 +1,5 @@
+print("Welcome to the two numbers calculator!")
+First = float(input("Enter your first number: "))
+Second = float(input("Enter the second number: "))
+Sum = First + Second
+print("The sum of the numbers is: ", str(Sum))
